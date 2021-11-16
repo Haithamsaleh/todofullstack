f2b51bb4e3cda644843d095ac87d9f82528001ca
Haithamsaleh <Haitham.saleh.elq.js@tuwaig.edu.sa>
2021-11-16 19:15:50 +0300

    start backend

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const { application } = require("express");
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`server on ${PORT}`);
+});
